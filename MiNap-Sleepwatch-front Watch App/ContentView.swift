2975b164d902282b9cbf891819a7289f31c9027e
jeonghin <jeonghin@gmail.com>
2023-07-03 13:14:11 -0400

    Added Transition

--- MiNap-Sleepwatch-front Watch App/ContentView.swift
+++ MiNap-Sleepwatch-front Watch App/ContentView.swift
@@ -2,31 +2,104 @@
 //  ContentView.swift
 //  MiNap-Sleepwatch-front Watch App
 //
-//  Created by Quishi Zhao on 6/28/23.
+//  Created by Jeong Hin Chin on 7/3/23.
 //
-
-/// ******** Just a practice view here, will not be used for our demo later*********
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var showSleepScreen = true
+    
     var body: some View {
         VStack {
-
-            Image("test_sleep")
-                        .resizable()
-                        .scaledToFit()
-            
-            Button(action: { print("hello") }, label: { Text("Sleep") })
+            if showSleepScreen {
+                SleepScreen(showSleepScreen: $showSleepScreen)
+                
+            } else {
+                WakeupScreen(showSleepScreen: $showSleepScreen)
+                
+            }
         }
-        .padding()
     }
 }
+
+struct SleepScreen: View {
+    @Binding var showSleepScreen: Bool
+    @State private var animate = false
+    @State private var showRecordedMessage = false
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "bed.double.fill")
+                .font(.system(size: 100))
+                .rotationEffect(Angle.degrees(animate ? 360 : 0))
+            
+            Text("Sleep")
+                .font(.title)
+            
+            Button("Go to sleep") {
+                animate = true
+                
+                // Display the recorded message
+                showRecordedMessage = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        showSleepScreen = false
+                        WKInterfaceController.reloadRootPageControllers(withNames: ["WakeupScreen"], contexts: nil, orientation: .vertical, pageIndex: 0)
+                    }
+                }
+            }
+            
+            if showRecordedMessage {
+                Text("Recording...")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
+
+struct WakeupScreen: View {
+    @Binding var showSleepScreen: Bool
+    @State private var animate = false
+    @State private var showRecordedMessage = false
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "sunrise.fill")
+                .font(.system(size: 100))
+                .rotationEffect(Angle.degrees(animate ? 360 : 0))
+            
+            Text("Wake up")
+                .font(.title)
+            
+            Button("Wake up") {
+                animate = true
+                
+                // Display the recorded message
+                showRecordedMessage = true
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        showSleepScreen = true
+                        WKInterfaceController.reloadRootPageControllers(withNames: ["SleepScreen"], contexts: nil, orientation: .vertical, pageIndex: 0)
+                    }
+                }
+            }
+            
+            if showRecordedMessage {
+                Text("Recording...")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
-
-
